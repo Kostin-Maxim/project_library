bbbbee8308faa278537547082aef53f886fdb42e
Maxim Kostin <maxim.kostin.1999@gmail.com>
2026-08-18 18:58:57 +0500

    Тестово накидал стили

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from urllib import request
+
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
