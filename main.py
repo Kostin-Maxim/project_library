99e5ddd82e81863be0292c2c2c1489d6a437481c
Maxim Kostin <maxim.kostin.1999@gmail.com>
2026-08-19 16:06:16 +0500

    Добавил страницы + возможность перехода по ним

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html", context={})
+    return templates.TemplateResponse(request, "index.html", {})
+
+@app.get("/games", response_class=HTMLResponse)
+def gamespage(request: Request):
+    return templates.TemplateResponse(request, "games.html", {})
+
+@app.get("/books", response_class=HTMLResponse)
+def bookspage(request: Request):
+    return templates.TemplateResponse(request, "books.html", {})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
